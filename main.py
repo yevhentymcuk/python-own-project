f339fad1ebd3054df516e33e286787ce3cc2cd7d
Levi <planetar2024@gmail.com>
2024-11-09 14:20:55 +0200

    sdfsd

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from crypt import methods
+from email.policy import default
+
 from flask import Flask, request, session, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import check_password_hash
 import os
+from datetime import datetime
 
 from sqlalchemy.testing.suite.test_reflection import users
 
@@ -32,9 +35,17 @@ class User(db.Model):
         return check_password_hash(self.password, password)
 
 
+class News(db.Model):
+    __tablename__ = 'News'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    image = db.Column(db.String(255), nullable=False)
+    text = db.Column(db.Text(), nullable=False)
+    created_on = db.Column(db.Date(), default=datetime.utcnow())
+    deleted = db.Column(db.Boolean, default=False)
 
-with app.app_context():
-    db.create_all()
+# with app.app_context():
+#     db.create_all()
 
 # === ROUTES ===================================================================================
 
@@ -43,10 +54,16 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/about')
-def about():
-    # код ...
-    return render_template('about.html')
+@app.route('/news')
+def news():
+    page = request.args.get('page', 1, type=int)
+    list_news = News.query.paginate(page=page, per_page=6)
+
+    for item in list_news:
+        if len(item.text) > 200:
+            item.text = item.text[:200] + '...'
+
+    return render_template('news.html', list_news=list_news)
 
 
 @app.route('/contact')
@@ -76,13 +93,13 @@ def login():
         user = User.query.filter_by(email=email).first()
 
         if not user:
-            massage = 'неправильний Email!'
+            massage = 'Неправильний Email!'
         else:
             if user.check_password(password):
                 session[SESSION_USER_ID] = user.id
                 return redirect('/')
 
-            massage = 'неправильний пароль!'
+            massage = 'Неправильний пароль!'
 
     return render_template('login.html', massage=massage)
 
